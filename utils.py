425b0dc16d577f27fc903760e7923846fa3b17a6
osm3000 <omar.samir3000@gmail.com>
2017-11-15 15:59:50 +0100

    Adding new utilities

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from sklearn.utils import shuffle
+import numpy as np
+
 def getbatch(*args, **kwargs):
     """
     Give it any number of arguments
@@ -27,3 +29,13 @@ def get_random_batch(*args, **kwargs):
     for argument in args_new:
         output_list.append(argument[i:i + min_len])
     return output_list
+
+def sample(preds, temperature=1.0):
+    # helper function to sample an index from a probability array
+    preds = np.asarray(preds).astype('float64')
+    # preds = np.log(preds) / temperature
+    preds = preds / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
